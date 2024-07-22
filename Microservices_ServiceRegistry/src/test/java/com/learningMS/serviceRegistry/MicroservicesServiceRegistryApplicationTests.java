a6500b6b76ef2a1e19a30ac025f53cc782776a19
Dhanjeet Kumar Thakur <68640018+DhanjeetKumarThakur@users.noreply.github.com>
2024-07-22 22:07:13 +0530

    Micro service Service Register
    Uploaded the Micro service Service Register Repository. Service Registry is where all the micro services will get registered including API Gateway.

---
+++ Microservices_ServiceRegistry/src/test/java/com/learningMS/serviceRegistry/MicroservicesServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningMS.serviceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
